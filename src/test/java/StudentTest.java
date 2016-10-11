adba016a99313a842cdec26bf1e9dc77a3835f6d
Daniel Sjösten <dansjo_87@hotmail.com>
2016-10-11 12:37:13 +0200

    Added tests to StudentTest

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -2,11 +2,61 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import otherclasses.Student;
+
 public class StudentTest {
 
+	
+	Student stud = new Student("Daniel", "Sjösten", 29, 'M', 1, 2, 3);
+	Student stud1 = new Student("Kalle", "Sjösten", 28, 'M');
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFirstName() {
+		assertEquals(stud.getFirstName(), "Daniel");
 	}
+	
+	@Test
+	public void testLastName() {
+		assertEquals(stud.getLastName(), "Sjösten");
+	}
+	
+	@Test
+	public void testAge() {
+		assertEquals(stud.getAge(), 29);
+	}
+	
+	@Test
+	public void testGender() {
+		assertEquals(stud.getGender(), 'M');
+	}
+	
+	@Test
+	public void testFirstGrade() {
+	stud.setFirstGrade(1);
+	stud.getFirstGrade();
+		assertEquals(stud.getSecondGrade(), stud.getFirstGrade());
+
+	}
+	
+	@Test
+	public void testSecondGrade() {
+		stud.setSecondGrade(2);
+		stud.getSecondGrade();
+		assertEquals(stud.getSecondGrade(), 2.0);
+		
+	}
+	
+	@Test
+	public void testThirdGrade() {
+		stud.setThirdGrade(3);
+		stud.getThirdGrade();
+		assertEquals(stud.getThirdGrade(), 3.0);
+	}
+	
+//	@Test
+//	public void testThirdGrade() {
+//		assertEquals(stud.getThirdGrade(), 3.0);
+//	}
 
 }
