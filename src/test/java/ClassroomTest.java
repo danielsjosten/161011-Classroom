72bc797fe6533f60d8e2fbd4f469f788a98133ef
Daniel Sjösten <dansjo_87@hotmail.com>
2016-10-13 08:09:16 +0200

    Added tests to classroomtest

--- src/test/java/ClassroomTest.java
+++ src/test/java/ClassroomTest.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 
@@ -8,51 +8,29 @@ import otherclasses.Classroom;
 import otherclasses.Student;
 
 public class ClassroomTest {
-	
-	private ArrayList<Student> student;
-	Classroom cr = new Classroom("Test16", "HT16", student);
+	ArrayList<Student> students = new ArrayList<Student>();
+	Classroom cr = new Classroom("Test16", "HT16", students);
+	Student student1 = new Student("Danne", "Sjösten", 28, 'M', 8.2, 9.0, 6.0);
 
 	@Test
 	public void testclassRoomName() {
 		cr.setClassroomName("Test17");
 		assertEquals(cr.getClassroomName(), "Test17");
 	}
-	
+
 	@Test
 	public void testTerm() {
 		cr.setClassroomTerm("VT17");
 		assertEquals(cr.getClassroomTerm(), "VT17");
 	}
-	
-	@Test
-	public void testStudent() {
-		student.add
-		assertEquals(stud.getAge(), 29);
-		//assertEquals(stud.getAge(), 1);
-	}
-	
-	@Test
-	public void testGender() {
-		assertEquals(stud.getGender(), 'M');
-	}
-	
-	@Test
-	public void testFirstGrade() {
-		stud.setFirstGrade(1);
-		assertEquals(stud.getFirstGrade(), 1, 0.00001);
 
-	}
-	
 	@Test
-	public void testSecondGrade() {
-		stud.setSecondGrade(2);
-		assertEquals(stud.getSecondGrade(), 2, 0.000001);
-			}
-	
-	@Test
-	public void testThirdGrade() {
-		stud.setThirdGrade(3);
-		assertEquals(stud.getThirdGrade(), 3, 0.000001);
+	public void setAndGetStudents() {
+		cr.addANewStudent(student1);
+		cr.setStudents(students);
+		cr.getStudents();
+
+
 	}
 
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -6,84 +6,83 @@ import otherclasses.Student;
 
 public class StudentTest {
 
-	
 	Student stud = new Student("Daniel", "Sjösten", 29, 'M', 1, 2, 3);
 	Student stud1 = new Student("Kalle", "Sjösten", 28, 'M');
-	
-	
+
 	@Test
 	public void testFirstName() {
 		assertEquals(stud.getFirstName(), "Daniel");
 	}
-	
+
 	@Test
 	public void testLastName() {
 		assertEquals(stud.getLastName(), "Sjösten");
 	}
-	
+
 	@Test
 	public void testAge() {
 		assertEquals(stud.getAge(), 29);
-		//assertEquals(stud.getAge(), 1);
+		// assertEquals(stud.getAge(), 1);
 	}
-	
+
 	@Test
 	public void testGender() {
 		assertEquals(stud.getGender(), 'M');
 	}
-	
+
 	@Test
 	public void testFirstGrade() {
 		stud.setFirstGrade(1);
 		assertEquals(stud.getFirstGrade(), 1, 0.00001);
 
 	}
-	
+
 	@Test
 	public void testSecondGrade() {
 		stud.setSecondGrade(2);
 		assertEquals(stud.getSecondGrade(), 2, 0.000001);
-			}
-	
+	}
+
 	@Test
 	public void testThirdGrade() {
 		stud.setThirdGrade(3);
 		assertEquals(stud.getThirdGrade(), 3, 0.000001);
 	}
-	
+
 	@Test
 	public void testAverageGrade() {
-		double average = (stud.getFirstGrade() + stud.getSecondGrade() + stud.getThirdGrade())/3;
+		double average = (stud.getFirstGrade() + stud.getSecondGrade() + stud.getThirdGrade()) / 3;
 		stud.setAverageGrade(average);
 		assertEquals(stud.getAverageGrade(), average, 0.000001);
-		
+
 	}
-	
+
 	@Test
 	public void testCalculateAverage1() {
-		double sum = stud.getFirstGrade() +stud.getSecondGrade() +stud.getThirdGrade();
-		double average = sum /3;
+		double sum = stud.getFirstGrade() + stud.getSecondGrade() + stud.getThirdGrade();
+		double average = sum / 3;
 		stud.setAverageGrade(average);
 		assertEquals(stud.calculateAverage(), average, 0.000001);
 	}
-	
+
 	@Test
 	public void testCalculateAverage2() {
-		double sum = stud.getFirstGrade() +stud.getSecondGrade() +stud.getThirdGrade();
-		double average = sum /3;
+		double sum = stud.getFirstGrade() + stud.getSecondGrade() + stud.getThirdGrade();
+		double average = sum / 3;
 		stud.setAverageGrade(average);
 		assertEquals(stud.calculateAverage(stud.getFirstGrade()), average, 0.000001);
 
 	}
-	
+
 	@Test
 	public void testCalculateAverage3() {
-		double sum = stud.getFirstGrade() +stud.getSecondGrade() +stud.getThirdGrade();
-		double average = sum /3;
+		double sum = stud.getFirstGrade() + stud.getSecondGrade() + stud.getThirdGrade();
+		double average = sum / 3;
 		stud.setAverageGrade(average);
-		assertEquals(stud.calculateAverage(stud.getFirstGrade(), stud.getSecondGrade(), stud.getThirdGrade()), average, 0.000001);
+		assertEquals(stud.calculateAverage(stud.getFirstGrade(), stud.getSecondGrade(), stud.getThirdGrade()), average,
+				0.000001);
 	}
-	
+
 	@Test
 	public void testHasClearedCourse() {
 		stud.setAverageGrade(6);
@@ -91,18 +90,18 @@ public class StudentTest {
 		stud.setAverageGrade(4);
 		assertEquals(stud.hasClearedTheCourse(), false);
 	}
-	
+
 	@Test
 	public void testToString() {
-		stud.setAverageGrade(6);
-		//assertEquals(stud.toString(), stud.setAverageGrade(8), true);
-		System.out.println(stud.toString());
-		assertEquals(stud.toString(), false, true);
-		System.out.println(stud.toString());
+
+		Student daniel = new Student("Daniel", "Sjösten", 29, 'M', 6, 6, 6);
+		Student kalle = new Student("Kalle", "Sjösten", 29, 'M', 4, 4, 4);
 		
+		
+		// assertEquals(stud.toString(), stud.setAverageGrade(8), true);
+	
+		assertEquals(daniel.toString(), true, true);
+		assertEquals(kalle.toString(), false, false);
 
 	}
-	
-
-
 }
